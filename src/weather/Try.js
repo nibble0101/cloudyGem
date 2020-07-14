54def4308430c84d00e48a258f7a2a0287a626d0
nibble0101 <mj.mawa@yahoo.com>
2020-07-14 11:57:19 +0300

    Added more styling to the weather information

--- src/weather/Try.js
+++ src/weather/Try.js
@@ -14,58 +14,58 @@ function spany(props) {
         <div>
           <img src={iconUrlObject.url} alt={dailyWeather.weather[0].main} />
         </div>
-        <div>
-          <span>
-            <span> Temperature </span>
-            <span>
-              <span> Morning </span>
-              <span> {dailyWeather.temp.morn} &#8457; </span>
-            </span>
-            <span>
-              <span> Day </span>
-              <span> {dailyWeather.temp.day} &#8457; </span>
-            </span>
-            <span>
-              <span> Evening </span>
-              <span> {dailyWeather.temp.eve} &#8457; </span>
-            </span>
-            <span>
-              <span> Night </span>
-              <span> {dailyWeather.temp.night} &#8457; </span>
-            </span>
-            <span>
-              <span> Minimum Temperature </span>
-              <span> {dailyWeather.temp.min} &#8457;</span>
-            </span>
-            <span>
-              <span> Maximum Temperature </span>
-              <span> {dailyWeather.temp.max} &#8457; </span>
-            </span>
-          </span>
-          <p>
+        <div className = "other">
+          <div className = "temperature">
+            <div className = "label"> Temperature </div>
+            <div className = "temperature-breakdown">
+              <span className = "morning">
+                <span> Morning </span>
+                <span> {dailyWeather.temp.morn} &#8451; </span>
+              </span>
+              <span className = "day">
+                <span> Day </span>
+                <span> {dailyWeather.temp.day} &#8451; </span>
+              </span>
+              <span className = "evening">
+                <span> Evening </span>
+                <span> {dailyWeather.temp.eve} &#8451; </span>
+              </span>
+              <span className = "night">
+                <span> Night </span>
+                <span> {dailyWeather.temp.night} &#8451; </span>
+              </span>
+              <span className = "min-temp">
+                <span> Min. Temperature </span>
+                <span> {dailyWeather.temp.min} &#8451;</span>
+              </span>
+              <span className = "max-temp">
+                <span> Max. Temperature </span>
+                <span> {dailyWeather.temp.max} &#8451; </span>
+              </span>
+            </div>
+          </div>
+          <div className = "humidity">
             <span> Humidity </span>
             <span> {dailyWeather.humidity} % </span>
-          </p>
-          <p>
+          </div>
+          <div className = "pressure">
             <span> Pressure </span>
             <span> {dailyWeather.pressure} hPa </span>
-          </p>
-          <p>
+          </div>
+          <div className = "wind-speed">
             <span> Wind Speed </span>
-            <span> {dailyWeather.wind_speed} mespane/sec </span>
-          </p>
-          <p>
+            <span> {dailyWeather.wind_speed} metres/sec </span>
+          </div>
+          <div className = "sunrise">
             <span> Sunrise </span>
             <span>
               {setSunTime(dailyWeather.sunrise, props.timeZoneOffset)}
             </span>
-          </p>
-          <p>
+          </div>
+          <div className = "sunset">
             <span> Sunset </span>
-            <span>
-              {setSunTime(dailyWeather.sunset, props.timeZoneOffset)}
-            </span>
-          </p>
+            <span>{setSunTime(dailyWeather.sunset, props.timeZoneOffset)}</span>
+          </div>
         </div>
       </div>
     </Zoom>
