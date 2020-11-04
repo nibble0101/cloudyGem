7e7689084acde083f8f8c3e6ffdda8d1d91d508a
nibble0101 <mj.mawa@gmail.com>
2020-11-04 13:02:54 +0300

    Modified error message

--- src/error/Error.js
+++ src/error/Error.js
@@ -3,23 +3,7 @@ import React from "react";
 function Error(props) {
   return (
     <div>
-      <p className="error-message dissapointed-face">
-        OOOPS!
-        <span role="img" aria-label="sad emoji">
-          😞
-        </span>
-      </p>
-      <p className="error-message">An Error occurred. Try again later!</p>
-      <p className="error-message">
-        You can also get in touch with me by opening an issue
-        <a
-          href="https://github.com/nibble0101/weather-app-revised"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Here
-        </a>
-      </p>
+      <p className="error-message">An Error occurred. Try again later</p>
     </div>
   );
 }
