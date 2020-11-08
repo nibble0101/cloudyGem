994e82bcc9c4f3e032409e758703f31ea283b8c3
nibble0101 <mjm.mawa@gmail.com>
2020-11-08 13:44:54 +0300

    Added label element for input

--- src/main/Search.js
+++ src/main/Search.js
@@ -1,20 +1,24 @@
 import React from "react";
 
-function Search(props){
-    return (
-        <div className="search">
-          <form onSubmit = {props.handleSubmit} id = "form">
-            <input
-              type="text"
-              id="search-box"
-              placeholder="Enter city..."
-              defaultValue = {props.value}
-              onChange = {props.handleChange}
-            />
-            <input type = "submit" className="submit"  />
-          </form> 
-        </div>
-      );
+function Search(props) {
+  return (
+    <div className="search">
+      <form onSubmit={props.handleSubmit} id="form">
+        <label htmlFor="search-box">
+          <span className="label-text">Enter Query</span>
+          <input
+            type="text"
+            id="search-box"
+            placeholder="Enter city..."
+            defaultValue={props.value}
+            onChange={props.handleChange}
+          />
+        </label>
+
+        <input type="submit" className="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Search;
