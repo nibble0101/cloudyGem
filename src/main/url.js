9e2cb372c523c5ee445135157d378e1d1336d1b4
nibble0101 <mjm.mawa@gmail.com>
2020-11-09 20:17:11 +0300

    Added a method for generating different uris

--- src/main/url.js
+++ src/main/url.js
@@ -1,29 +1,22 @@
-function GenerateUrl(lat, lon, key) {
-  this.baseUrl = "https://api.openweathermap.org/data/2.5/onecall";
+function GenerateUrl(lat, lon, query = "", key) {
+  this.oneCallBaseUrl = "https://api.openweathermap.org/data/2.5/onecall";
+  this.locationBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
   this.lat = lat;
   this.lon = lon;
+  this.query = query;
   this.units = "metric";
   this.key = key;
   this.excluded = ["current", "minutely", "hourly"];
-  this.url = function () {
-    return (
-      this.baseUrl +
-      "?" +
-      "lat=" +
-      this.lat +
-      "&" +
-      "lon=" +
-      this.lon +
-      "&" +
-      "units=" +
-      this.units +
-      "&" +
-      "exclude=" +
-      this.excluded.join(",") +
-      "&" +
-      "appid=" +
-      this.key
-    );
+  this.locationUrlByCoordinates = function () {
+    return `${this.locationBaseUrl}?lat=${this.lat}&lon=${this.lon}&appid=${this.key}`;
+  };
+  this.locationUrlByQuery = function () {
+    return `${this.locationBaseUrl}?q=${this.query}&appid=${this.key}`;
+  };
+  this.oneCallUrl = function () {
+    return `${this.oneCallBaseUrl}?lat=${this.lat}&lon=${this.lon}&units=${
+      this.units
+    }&exclude=${this.excluded.join(",")}&appid=${this.key}`;
   };
 }
 
